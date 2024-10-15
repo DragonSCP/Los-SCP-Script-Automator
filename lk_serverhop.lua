b5dd5e121646ee845037a919b5144dbfa0f040b6
DragonSCP <135724825+DragonSCP@users.noreply.github.com>
2024-10-15 11:56:07 -0300

    Update lk_serverhop.lua

--- lk_serverhop.lua
+++ lk_serverhop.lua
@@ -6,87 +6,69 @@ local S_T = game:GetService("TeleportService")
 local S_H = game:GetService("HttpService")
 
 local File = pcall(function()
-    AllIDs = S_H:JSONDecode(readfile("server-hop-temp.json"))
+	AllIDs = S_H:JSONDecode(readfile("server-hop-temp.json"))
 end)
 if not File then
-    table.insert(AllIDs, actualHour)
-    pcall(function()
-        writefile("server-hop-temp.json", S_H:JSONEncode(AllIDs))
-    end)
-end
+	table.insert(AllIDs, actualHour)
+	pcall(function()
+		writefile("server-hop-temp.json", S_H:JSONEncode(AllIDs))
+	end)
 
-local function CheckMoon()
-    local moon5 = "http://www.roblox.com/asset/?id=9709149431" -- Lua Cheia
-    local moon4 = "http://www.roblox.com/asset/?id=9709149052" -- Noite anterior à Lua Cheia
-    local moonreal = MoonTextureId()
-    local cofullmoonkothangbeo = "Bad Moon"
-    
-    if moonreal == moon5 or moonreal == moon4 then
-        if moonreal == moon5 then
-            cofullmoonkothangbeo = "Full Moon"
-        elseif moonreal == moon4 then
-            cofullmoonkothangbeo = "Next Night"
-        end
-    end
-    
-    return cofullmoonkothangbeo
 end
-
 local function TPReturner(placeId)
-    local Site
-    if foundAnything == "" then
-        Site = S_H:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. placeId .. '/servers/Public?sortOrder=Asc&limit=100'))
-    else
-        Site = S_H:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. placeId .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
-    end
-    local ID = ""
-    if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
-        foundAnything = Site.nextPageCursor
-    end
-    local num = 0
-    for i, v in pairs(Site.data) do
-        local Possible = true
-        ID = tostring(v.id)
-        if tonumber(v.maxPlayers) > tonumber(v.playing) then
-            for _, Existing in pairs(AllIDs) do
-                if num ~= 0 then
-                    if ID == tostring(Existing) then
-                        Possible = false
-                    end
-                else
-                    if tonumber(actualHour) ~= tonumber(Existing) then
-                        local delFile = pcall(function()
-                            delfile("server-hop-temp.json")
-                            AllIDs = {}
-                            table.insert(AllIDs, actualHour)
-                        end)
-                    end
-                end
-                num = num + 1
-            end
-            if Possible == true and CheckMoon() ~= "Bad Moon" then
-                table.insert(AllIDs, ID)
-                wait()
-                pcall(function()
-                    writefile("server-hop-temp.json", S_H:JSONEncode(AllIDs))
-                    wait()
-                    S_T:TeleportToPlaceInstance(placeId, ID, game.Players.LocalPlayer)
-                end)
-                wait(4)
-            end
-        end
-    end
+	local Site;
+	if foundAnything == "" then
+		Site = S_H:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. placeId .. '/servers/Public?sortOrder=Asc&limit=100'))
+	else
+		Site = S_H:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. placeId .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
+	end
+	local ID = ""
+	if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+		foundAnything = Site.nextPageCursor
+	end
+	local num = 0;
+	for i,v in pairs(Site.data) do
+		local Possible = true
+		ID = tostring(v.id)
+		if tonumber(v.maxPlayers) > tonumber(v.playing) then
+			for _,Existing in pairs(AllIDs) do
+				if num ~= 0 then
+					if ID == tostring(Existing) then
+						Possible = false
+					end
+				else
+					if tonumber(actualHour) ~= tonumber(Existing) then
+						local delFile = pcall(function()
+							delfile("server-hop-temp.json")
+							AllIDs = {}
+							table.insert(AllIDs, actualHour)
+						end)
+					end
+				end
+				num = num + 1
+			end
+			if Possible == true then
+				table.insert(AllIDs, ID)
+				wait()
+				pcall(function()
+					writefile("server-hop-temp.json", S_H:JSONEncode(AllIDs))
+					wait()
+					S_T:TeleportToPlaceInstance(placeId, ID, game.Players.LocalPlayer)
+				end)
+				wait(4)
+			end
+		end
+	end
 end
-
 local module = {}
 function module:Teleport(placeId)
-    while wait() do
-        pcall(function()
-            TPReturner(placeId)
-            if foundAnything ~= "" then
-                TPReturner(placeId)
-            end
-        end)
-    end
+	while wait() do
+		pcall(function()
+			TPReturner(placeId)
+			if foundAnything ~= "" then
+				TPReturner(placeId)
+			end
+		end)
+	end
 end
 return module
